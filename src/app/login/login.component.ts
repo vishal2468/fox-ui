25d15d847a45d50491bf33b2ad1119562d4a5cff
vishal2468 <vishalpoddar2468@gmail.com>
2023-11-01 00:02:03 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  user = {
+    userName: '',
+    password: '',
+  };
+  constructor(private http: HttpClient,private router: Router) {}
 
+  submitForm() {
+    // Add your form submission logic here
+    console.log(this.user);
+    // You can send this data to a server or perform any other action.
+    const url = 'http://localhost:8080/login';
+
+    this.http.post(url, this.user).subscribe(
+      (response) => {
+        console.log('Login successfully:', response);
+      },
+      (error) => {
+        console.error('Error logging in', error);
+        // Handle error response from the external service
+      }
+    );
+  }
 }
