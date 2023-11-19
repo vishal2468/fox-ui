a33d4a1200a259327f19c2900bd10896fadfe555
Vishal Kumar Poddar <vpoddar@opentext.com>
2023-11-19 10:01:03 +0530

    added Oauth button on patient dashboard

--- src/app/patient-dashboard/patient-dashboard.component.ts
+++ src/app/patient-dashboard/patient-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-patient-dashboard',
   templateUrl: './patient-dashboard.component.html',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 export class PatientDashboardComponent {
   selectedDropdownValue?: string;
   userName?: string;
+
+  constructor(private http: HttpClient) {}
 
   onDropdownValueChange(value: string): void {
     this.selectedDropdownValue = value;
@@ -18,5 +21,20 @@ export class PatientDashboardComponent {
       selectedDropdownValue: this.selectedDropdownValue,
       userName: this.userName,
     });
+  }
+  onOAuthButtonClick(): void {
+    // Call your external OAuth API here
+    const oauthApiUrl = 'your_oauth_api_url';
+
+    this.http.get(oauthApiUrl).subscribe(
+      (response) => {
+        // Handle the OAuth API response
+        console.log('OAuth API response:', response);
+      },
+      (error) => {
+        // Handle errors
+        console.error('Error calling OAuth API:', error);
+      }
+    );
   }
 }
